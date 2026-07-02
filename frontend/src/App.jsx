f5c398fdb191b32ac909cdcf73eccd6f520e0c8b
Jaya Maurya <jayamaurya247@gmail.com>
2026-07-02 14:22:42 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = 'http://localhost:5000/api/tasks';
+const API_URL = 'https://task-tracker-jmoa.onrender.com/api/tasks';
 
 function App() {
   const [tasks, setTasks] = useState([]);
